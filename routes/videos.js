950bd065baf8fd79ef2f637916dd9cd38755a011
node-toso-api <alisonsmithaccy@gmail.com>
2019-09-29 16:27:07 +0100

    corrected api routre

--- routes/videos.js
+++ routes/videos.js
@@ -4,9 +4,9 @@ const router = express.Router();
 
 /* GET home page. */
 router.get("/videos", async (req, res) => {
-  const videos = await fs.readFileSync("./data/videos.json");
+  const response = await fs.readFileSync("./data/videos.json");
   // console.log(videos);
-  res.status(200).send(JSON.parse(videos));
+  res.status(200).send(JSON.parse(response));
 });
 
 module.exports = router;
